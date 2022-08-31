3a84adecd71961a2b8330872f1f9089c7a041042
Gaveen Aperano Mouloungui <gaveen@MacBook-Pro-de-Gaveen.local>
2022-08-31 10:56:42 +0200

    add dbn

--- include/db_connect.c
+++ include/db_connect.c
@@ -40,7 +40,7 @@ void insert_weather(char **json_string)
     json_string[10], json_string[11], json_string[12], json_string[13],\
     json_string[14], json_string[17], json_string[18]);
 
-    PGconn *conn = PQconnectdb("host=localhost user=gavin password=password");
+    PGconn *conn = PQconnectdb("host=localhost user=Weather_Report password=password");
     PGresult* res;
     if(conn != NULL)
     {
@@ -61,7 +61,7 @@ void insert_weather(char **json_string)
 
 int go_weather(char **json_string) {
     
-    PGconn *conn = PQconnectdb("host=localhost user=gavin password=password");
+    PGconn *conn = PQconnectdb("host=localhost user=Weather_Report password=password");
 
     if (PQstatus(conn) == CONNECTION_BAD) {
         
